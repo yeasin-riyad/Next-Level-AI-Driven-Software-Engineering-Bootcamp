fb07217c8827ce50ffd02a205d7879a62147c186
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-05-04 11:52:28 +0600

    keyOfConstraintWithGenerics.ts

---
+++ Be_A_TypeScript_Technocrat/Explore_Advance_Types_Of_Typescript/keyOfConstraintWithGenerics.ts
@@ -0,0 +1,55 @@
+// keyof : type operator
+
+type RichPeoplesVehicle = {
+  car: string; // key: value
+  bike: string;
+  cng: string;
+};
+
+type MyVehicle1 = "bike" | "car" | "cng";
+// Or
+type Myvehicle2 = keyof RichPeoplesVehicle;
+
+const myVehicle: Myvehicle2 = "ship";
+
+type User1 = {
+  id: number;
+  name: string;
+  address: {
+    city: string;
+  };
+};
+
+const user: User1 = {
+  id: 222, // key: value
+  name: "Mezba",
+  address: {
+    city: "ctg",
+  },
+};
+
+// //const myId = user.id;
+// const myId = user["id"];
+// const myName = user["name"];
+// const address = user["address"];
+
+//console.log({ myId, myName, address });
+
+const getPropertyFromObj = <X>(obj: X, key: keyof X) => {
+  return obj[key];
+};
+
+// const result1 = getPropertyFromObj(user, "emni");
+// console.log(result);
+
+const product = {
+  brand: "HP",
+};
+
+const student = {
+  id: 123,
+  class: "four",
+};
+
+const result23 = getPropertyFromObj(product, "brand");
+const result3 = getPropertyFromObj(student, "id");
