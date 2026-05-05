e65733323757abd48f0390b76d842e6b1a7e001e
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-05-05 20:32:06 +0600

    Type_Assertion.ts

--- Be_A_TypeScript_Technocrat/TypeScript_Comprehensive_Practice_Tasks/Type_Assertion.ts
+++ Be_A_TypeScript_Technocrat/TypeScript_Comprehensive_Practice_Tasks/Type_Assertion.ts
@@ -1,1 +1,18 @@
 // Concepts: Type Assertion (as), unknown type
+
+// Task 4: Type Assertion MEDIUM
+// Concepts: Type Assertion (as), unknown type
+// Scenario: You receive a value from a 3rd-party library typed as unknown. You are
+// certain it's a string and need to manipulate it.
+
+// let secretValue: unknown = "typescript is awesome";
+
+// Instructions:
+// Create a variable upperValue.
+// Assign secretValue to it using Type Assertion.
+// Call .toUpperCase() on the resulting value.
+// Hint: Use the value as string syntax.
+
+let secretValue: unknown = "typescript is awesome";
+let upperValue: string = secretValue as string;
+console.log(upperValue.toUpperCase());
