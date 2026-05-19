90bffb8cd8bc67b674a13a49430954a767babf8b
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-05-19 21:32:25 +0600

    Send Response Utility Creation

--- Be_A_Node_Express_Expert/Express_Middleware_JWT_And_Role-based-Authorization/src/modules/user/user.controller.ts
+++ Be_A_Node_Express_Expert/Express_Middleware_JWT_And_Role-based-Authorization/src/modules/user/user.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import { createUserIntoDB, deleteUserByIdFromDB, getAllUsersFromDB, getUserByIdFromDB, updateUserByIdFromDB } from "./user.service";
 import type { IUser } from "./user.interface";
+import sendResponse from "../../utility/sendResponse";
 
 export const createUser=async(req:Request,res:Response)=>{
    try{
@@ -11,15 +12,18 @@ export const createUser=async(req:Request,res:Response)=>{
      
     const result =await createUserIntoDB(payload);
     delete result.rows[0].password; // Remove password from the response
-    res.status(201).json({
-        "message":"Data received successfully",
-        "data":result.rows[0]
-    })
-   }catch(error){
+ sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "User Created successfully!",
+      data: result.rows[0],
+    });   }catch(error){
     // console.error('Error inserting data into PostgreSQL database:', error);
-    res.status(500).json({
-        "message":error instanceof Error ? error.message : "Internal Server Error"
-    })
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: error instanceof Error ? error.message : "Internal Server Error",
+    });
    }
 
 }
@@ -30,15 +34,19 @@ export const getAllUsers=async(req:Request,res:Response)=>{
         result.rows.forEach((row) => {
             delete row.password;
         });
-        res.status(200).json({
-            "message":"Users retrieved successfully",
-            "data":result.rows
-        })
+        sendResponse(res, {
+          statusCode: 200,
+          success: true,
+          message: "Users retrieved successfully",
+          data: result.rows
+        });
     }catch(error){
-        // console.error('Error retrieving users from PostgreSQL database:', error);
-        res.status(500).json({
-            "message":error instanceof Error ? error.message : "Internal Server Error"
-        })
+      
+        sendResponse(res, {
+          statusCode: 500,
+          success: false,
+          message: error instanceof Error ? error.message : "Internal Server Error",
+        });
     }
 }
 
@@ -47,21 +55,28 @@ export const getUserById=async(req:Request,res:Response)=>{
     try{
         const result =await getUserByIdFromDB(userId);
         if(result.rows.length === 0){
-            res.status(404).json({
-                "message":"User not found"
-            })
+            sendResponse(res, {
+              statusCode: 404,
+              success: false,
+              message: "User not found"
+            });
         }else{
             delete result.rows[0].password; // Remove password from the response
-            res.status(200).json({
-                "message":"User retrieved successfully",
-                "data":result.rows[0]
-            })
+            sendResponse(res, {
+              statusCode: 200,
+              success: true,
+              message: "User retrieved successfully",
+              data: result.rows[0]
+            });
+         
         }
     }catch(error){
         // console.error('Error retrieving user from PostgreSQL database:', error);
-        res.status(500).json({
-            "message":error instanceof Error ? error.message : "Internal Server Error"
-        })
+        sendResponse(res, {
+          statusCode: 500,
+          success: false,
+          message: error instanceof Error ? error.message : "Internal Server Error",
+        });
     }
 };
 
@@ -75,21 +90,28 @@ export const updateUserById=async(req:Request,res:Response)=>{
     try{
         const result=await updateUserByIdFromDB(payload)
         if(result.rows.length === 0){
-            res.status(404).json({
-                "message":"User not found"
-            })
+            sendResponse(res, {
+              statusCode: 404,
+              success: false,
+              message: "User not found"
+            });
         }else{
             delete result.rows[0].password; // Remove password from the response
-            res.status(200).json({
-                "message":"User updated successfully",
-                "data":result.rows[0]
-            })
+            sendResponse(res, {
+              statusCode: 200,
+              success: true,
+              message: "User updated successfully",
+              data: result.rows[0]
+            });
         }
     }catch(error){
         // console.error('Error updating user in PostgreSQL database:', error);
-        res.status(500).json({
-            "message":error instanceof Error ? error.message : "Internal Server Error"
-        })
+        sendResponse(res, {
+          statusCode: 500,
+          success: false,
+          message: error instanceof Error ? error.message : "Internal Server Error",
+        });
+    
     }
 };
 
@@ -98,19 +120,26 @@ export const deleteUserById=async(req:Request,res:Response)=>{
     try{
         const result = await deleteUserByIdFromDB(userId);
         if(result.rows.length === 0){
-            res.status(404).json({
-                "message":"User not found"
-            })
+            sendResponse(res, {
+              statusCode: 404,
+              success: false,
+              message: "User not found"
+            });
+        
         }else{
-            res.status(200).json({
-                "message":"User deleted successfully",
-                "data":result.rows[0]
-            })
+            sendResponse(res, {
+              statusCode: 200,
+              success: true,
+              message: "User deleted successfully",
+              data: result.rows[0]
+            });
         }
     }catch(error){
         // console.error('Error deleting user from PostgreSQL database:', error);
-        res.status(500).json({
-            "message":error instanceof Error ? error.message : "Internal Server Error"
-        })
+        sendResponse(res, {
+          statusCode: 500,
+          success: false,
+          message: error instanceof Error ? error.message : "Internal Server Error",
+        });
     }
 };
